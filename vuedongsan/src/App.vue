ee5dead8e5548c79b67bd845a78224ae8c3bcf59
iafan1229 <home0@naver.com>
2022-12-30 15:38:20 +0900

    update(*) vus-ts 프로젝트 시작

---
+++ vuedongsan/src/App.vue
@@ -0,0 +1,71 @@
+<template>
+  <div>
+    <ul class="list">
+      <li v-for="(a, i) in list" :key="i">
+        <img :src="list[i].image" />
+        <span class="title" @click="handleClick(i)">{{ list[i].title }}</span>
+        <span>price: {{ list[i].price }}</span>
+        <span>신고수: {{ alarm[i] }}</span>
+        <button @click="increase(i)">증가</button>
+      </li>
+    </ul>
+    <vueModal :data="list" :press="press" v-if="show" @close="show = false" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import list from "./assets/data";
+import vueModal from "./components/VueModal.vue";
+
+export default defineComponent({
+  data() {
+    return {
+      press: 0,
+      show: false,
+      alarm: Array(6).fill(0),
+      list,
+    };
+  },
+  methods: {
+    increase(i: number) {
+      this.alarm[i] += 1;
+    },
+    handleClick(i: number) {
+      (this.press = i), (this.show = true);
+    },
+  },
+  components: {
+    vueModal,
+  },
+});
+</script>
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+}
+ul {
+  list-style: none;
+}
+.list {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 30px;
+  > li {
+    width: calc(100% / 3 - 30px);
+    text-align: center;
+    img {
+      width: 100%;
+    }
+    span {
+      &.title {
+        color: rgb(111, 110, 110);
+        cursor: pointer;
+      }
+      display: block;
+      text-align: center;
+    }
+  }
+}
+</style>
---
+++ vuedongsan/src/components/VueModal.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <ul class="modal">
+      <li><img :src="data[press].image" /></li>
+      <li>{{ data[press].title }}</li>
+      <li>{{ data[press].content }}</li>
+      <li>{{ data[press].price }}원</li>
+      <li><button @click="$emit('close')">닫기</button></li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  props: {
+    data: Array,
+    press: Number,
+  },
+});
+</script>
+<style lang="scss">
+.modal {
+  width: 300px;
+  min-height: 300px;
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+  padding: 30px;
+  background: #fff;
+  border: 1px solid;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 5px;
+  justify-content: center;
+  align-items: center;
+  img {
+    width: 100%;
+  }
+}
+</style>
