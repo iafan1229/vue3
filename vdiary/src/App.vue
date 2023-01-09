3847e16cd9fda257f0b509b4b55fdf6357b36d8e
hayoung <home0@naver.com>
2023-01-09 16:10:55 +0900

    New(*) : 건강 관리일지 프로젝트 시작 1/9

---
+++ vdiary/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+<div>hello {{ a }}</div>
+</template>
+
+<script setup lang="ts">
+import { onMounted, ref, watch } from "@vue/runtime-core";
+const a = ref(3);
+onMounted(()=>{
+  console.log('hi') 
+  a.value=4;
+})
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
